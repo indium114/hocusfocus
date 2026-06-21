c88838ca10b2b6d1c8e20873a098cb45e630fdbb
indium114 <156162907+indium114@users.noreply.github.com>
2026-06-21 11:30:30 +0200

    chore: initial rust stuff

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "rust devshell and package, created by scaffolder";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "rust-devshell";
+
+          packages = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+            rust-analyzer
+            clippy
+            pkg-config
+          ];
+        };
+
+        packages.hocusfocus = pkgs.rustPlatform.buildRustPackage {
+          name = "hocusfocus";
+          version = "2.0.0";
+
+          src = ./.;
+
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+
+        apps.hocusfocus = {
+          type = "app";
+          program = "${self.packages.${pkgs.stdenv.hostPlatform.system}.hocusfocus}/bin/hocusfocus";
+        };
+      });
+}
