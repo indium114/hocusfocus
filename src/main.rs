00f2372ac6b169c60176cc049cacd96eaa40a47e
indium114 <156162907+indium114@users.noreply.github.com>
2026-06-21 11:45:17 +0200

    fix: use args[1] as string

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() > 1 {
-        match args[1] {
+        match args[1].as_str() {
             "help" => {
                 println!("called help")
             }
